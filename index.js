470f7888d66399ee31fa19c9d7073b4fa2feab15
titevar <59051464+titevar@users.noreply.github.com>
2020-04-06 19:33:38 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const app = express();
-const http = require('http').Server(app);
+const http = require('http').createServer();
 const io = require('socket.io')(http);
 const port = process.env.PORT || 8080;
 
